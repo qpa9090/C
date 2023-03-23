5de7f3e257a3574b64b038c64d472879264dca20
qpa9090 <127387974+qpa9090@users.noreply.github.com>
2023-03-23 11:29:53 +0900

    Add files via upload

---
+++ C_4/double1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    double x,y,result;
+
+    printf("두개의 실수를 입력하시오: ");
+    scanf("%lf %lf", &x, &y);
+
+    result=x+y;         //덧셈 연산을 해서 결과를 result에 대입
+    printf("%lf + %lf = %lf\n",x,y,result);
+    result=x-y;
+    printf("%lf - %lf = %lf\n",x,y,result);
+    result=x*y;
+    printf("%lf * %lf = %lf\n",x,y,result);
+    result=x/y;
+    printf("%lf / %lf = %lf\n",x,y,result);
+    return 0;
+}    
